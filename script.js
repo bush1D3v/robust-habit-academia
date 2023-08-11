406beac75086f303992ec804dce4f72f129b756b
bush1D3v <victor.jose.lopes.navarro@gmail.com>
2023-08-11 18:42:49 -0300

    Adicionando verificação de dados e envio de json

--- script.js
+++ script.js
@@ -19,3 +19,54 @@ if(target.length){
         animeScroll()
     })
 }
+
+const form = document.querySelector('form')
+const nameInput = document.getElementById('name')
+const phoneInput = document.getElementById('phone')
+const nameDiv = document.getElementById('nome')
+const phoneDiv = document.getElementById('telefone')
+const errorClass = 'error'
+
+function nameValidation(){
+    const nameValue = nameInput.value.trim()
+
+    if(nameValue.length < 5){
+        nameDiv.classList.add(errorClass)
+        return false
+    }else{
+        nameDiv.classList.remove(errorClass)
+        return true
+    }
+}
+
+function phoneValidation(){
+    const phoneValue = phoneInput.value.trim()
+
+    if(phoneValue.length < 10){
+        phoneDiv.classList.add(errorClass)
+        return false
+    }else{
+        phoneDiv.classList.remove(errorClass)
+        return true
+    }
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    nameValidation()
+    phoneValidation()
+    if(nameValidation() && phoneValidation()){
+        console.log({
+            nome: nameInput.value.trim(),
+            telefone: phoneInput.value.trim()
+        });
+    }
+  })
+
+  nameInput.addEventListener('keydown', () => {
+    nameValidation()
+  })
+
+  phoneInput.addEventListener('keydown', () => {
+    phoneValidation()
+  })
